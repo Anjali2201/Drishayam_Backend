d1c51c8d244e40fa3ef70a567225c494bfca6c0a
Anjali2201 <anjalikushwaha031@gmail.com>
2023-06-23 01:08:10 +0530

    test

--- app.py
+++ app.py
@@ -2,66 +2,77 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from flask_cors import CORS
+import warnings
+
 
 app = Flask(__name__)
 CORS(app)
 
-# Load the TFLite model
-model_path = 'model.tflite'
-interpreter = tf.lite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
 
-# Get model input and output details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+# # Load the TFLite model
+# model_path = 'model.tflite'
+# interpreter = tf.lite.Interpreter(model_path=model_path)
+# interpreter.allocate_tensors()
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the name and age from the request
-    name = request.form.get('name')
-    age = request.form.get('age')
-    image_path = request.files["image"]
-    image_path.save('./Assets/uploaded_image.jpg')
-
-    image_new = "./Assets/uploaded_image.jpg"
-    image = tf.keras.preprocessing.image.load_img(image_new, target_size=(55, 94))
-    image = tf.keras.preprocessing.image.img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    image = image.astype('float32')
-    image /= 255.0  # Normalize the image
-    # Set the input tensor
-    interpreter.set_tensor(input_details[0]['index'], image)
-
-    # Run the inference
-    interpreter.invoke()
-
-    # Get the output tensor
-    output_data = interpreter.get_tensor(output_details[0]['index'])
+# # Get model input and output details
+# input_details = interpreter.get_input_details()
+# output_details = interpreter.get_output_details()
 
 
-    # Example: Assuming the output is a single value representing the prediction
-    prediction = float(output_data[0])
-    # convert into percentages with two decimal points
-    prediction_new = round(prediction * 100, 2)
 
 
-    # Create the response payload
-    status=""
-    if prediction > 0.5:
-        status="Normal Vision"
-    else :
-        status="Cataract"
-    response = {
-        'prediction': {
-            'name': name,
-            'age': age,
-            'status': status,
-            'probability': prediction_new,
-        }
-    }
-    # Return the response as JSON
-    print (response)
-    return jsonify(response)
+@app.route('/', methods=["GET"])
+def index():
+    return 'Hello World!'
+
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     # Get the name and age from the request
+#     name = request.form.get('name')
+#     age = request.form.get('age')
+#     image_path = request.files["image"]
+#     image_path.save('./Assets/uploaded_image.jpg')
+
+#     image_new = "./Assets/uploaded_image.jpg"
+#     image = tf.keras.preprocessing.image.load_img(image_new, target_size=(55, 94))
+#     image = tf.keras.preprocessing.image.img_to_array(image)
+#     image = np.expand_dims(image, axis=0)
+#     image = image.astype('float32')
+#     image /= 255.0  # Normalize the image
+#     # Set the input tensor
+#     interpreter.set_tensor(input_details[0]['index'], image)
+
+#     # Run the inference
+#     interpreter.invoke()
+
+#     # Get the output tensor
+#     output_data = interpreter.get_tensor(output_details[0]['index'])
+
+
+#     # Example: Assuming the output is a single value representing the prediction
+#     prediction = float(output_data[0])
+#     # convert into percentages with two decimal points
+#     prediction_new = round(prediction * 100, 2)
+
+
+#     # Create the response payload
+#     status=""
+#     if prediction > 0.5:
+#         status="Normal Vision"
+#     else :
+#         status="Cataract"
+#     response = {
+#         'prediction': {
+#             'name': name,
+#             'age': age,
+#             'status': status,
+#             'probability': prediction_new,
+#         }
+#     }
+#     # Return the response as JSON
+#     print (response)
+#     return jsonify(response)
 
 
 
@@ -83,6 +94,7 @@ def predict():
 #     return 'Success'
 
 if __name__ == '__main__':
+
     app.run(
         debug=True,
     )
